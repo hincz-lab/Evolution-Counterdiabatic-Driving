fb1d73a9f76fae2f83ee7fb2722a8468382ad7e9
Emily Dolson <EmilyLDolson@gmail.com>
2019-09-23 02:05:13 -0400

    Made script more user-friendly

--- analysis/draw_tesseract.R
+++ analysis/draw_tesseract.R
@@ -7,12 +7,24 @@ library(dplyr)
 library(graphlayouts)
 library(stringr)
 library(gganimate)
+library(readr)
+
+# Adjust this to choose file
+filename <- "pop_sizes.csv"
+
+# Adjust this to choose timepoint for snapshot
+time_point <- 10000
+
+# Adjust these to determine time range for animation
+lower_time_bound <- 10000
+upper_time_bound <- 20000
 
 # Helper for converting node ids to labels
 make_label <- function(x) {paste(as.character(as.binary(x-1, n=4)), collapse="")}
 # Helper for finding y coordinate based on node id
 get_y <- function(x){sum(as.vector(as.binary(x-1)))}
 
+# Deal with ugly ggraph interal stuff
 make_layout <- function() {
   adjmat <- read.csv("adj_mat.csv", header = FALSE)
   adjmat <- as.matrix(adjmat)
@@ -25,6 +37,7 @@ make_layout <- function() {
   return(layout)
 }
 
+# Sneakily add supplemental data to ggraph object
 add_data_to_graph <- function(layout, df) {
   spread_pop_df <- df %>% spread(generation, pop)
   new_layout <- layout %>% full_join(spread_pop_df) %>% gather(-name, -circular, -label, -.ggraph.orig_index, -.ggraph.index, -x, -y, key="time", value="pop")    
@@ -35,22 +48,33 @@ add_data_to_graph <- function(layout, df) {
   return(new_layout)
 }
 
-make_graph <- function() {
-  layout <- make_layout()  
-  ggraph(layout) + geom_edge_link() + geom_node_label(aes(label=label))
+# Read in file output by C++ code and convert it to correct format
+prepare_file <- function(filename){
+  pop_sizes <- read_csv(filename)
+  df <- pop_sizes %>% gather(-generation, key="name", value="pop")
+  df$name <- str_replace(df$name, "pop", "V")
+  df$name <- str_replace(df$name, "[:digit:]+", function(x){return(as.numeric(x)+1)})
+  return(df)
 }
 
-# Edit this file name to change the input file
-pop_sizes <- read_csv("../data/1/pop_sizes.csv")
-df <- pop_sizes %>% gather(-generation, key="name", value="pop")
+# Make an animation over time
+make_temporal_animation <- function(filename, lower_time_bound, upper_time_bound, frame_freq = 10) {
+  df <- prepare_file(filename)
+  layout <- make_layout()
+  data_layout <- add_data_to_graph(layout, df %>% filter(generation > lower_time_bound) %>% filter(generation < upper_time_bound) %>% filter(generation %% frame_freq == 0))
+  ggraph(data_layout) + geom_edge_link(start_cap = circle(5, 'mm'), end_cap = circle(5, 'mm')) + geom_node_circle(aes(r=sqrt(pop)/100), fill="white") + theme_graph(background = "white") + geom_node_text(aes(label=label)) + transition_time(time)
+}
 
-# Adjust the numbers in the calls to filter to adjust lower and upper bounds
-add_data_to_graph(layout, df %>% filter(generation > 10000) %>% filter(generation < 11000))
-df$name <- str_replace(df$name, "pop", "V")
-df$name <- str_replace(df$name, "[:digit:]+", function(x){return(as.numeric(x)+1)})
+# Make a figure of a single snapshot in time
+make_snapshot <- function(filename, time_point) {
+  df <- prepare_file(filename)
+  layout <- make_layout()
+  data_layout <- add_data_to_graph(layout, df %>% filter(generation == time_point))
+  ggraph(data_layout) + geom_edge_link(start_cap = circle(5, 'mm'), end_cap = circle(5, 'mm')) + geom_node_circle(aes(r=sqrt(pop)/100), fill="white") + theme_graph(background = "white") + geom_node_text(aes(label=label))  
+}
 
-layout <- make_layout()
-data_layout <- add_data_to_graph(layout, df %>% filter(generation > 10000) %>% filter(generation < 11000) %>% filter(generation %% 10 == 0))
-ggraph(new_layout) + geom_edge_link(start_cap = circle(5, 'mm'), end_cap = circle(5, 'mm')) + geom_node_circle(aes(r=sqrt(pop)/100), fill="white") + theme_graph(background = "white") + geom_node_text(aes(label=label)) + transition_time(time)
+# Uncomment this line to make a temporal animation
+# make_temporal_animation(filename, lower_time_bound, upper_time_bound)
 
-
+# This line makes a snapshot
+make_snapshot(filename, time_point)
