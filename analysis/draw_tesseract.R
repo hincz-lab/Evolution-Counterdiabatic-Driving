02e47d18c099b5c7917cbc75c69680b365557387
Emily Dolson <EmilyLDolson@gmail.com>
2019-09-24 01:51:27 -0400

    First stab at box and whiskers circles

--- analysis/draw_tesseract.R
+++ analysis/draw_tesseract.R
@@ -81,7 +81,35 @@ make_circle_overlay <- function(filename1, filename2, time_point) {
   df2 <- prepare_file(filename2)
   data_layout <- add_data_to_graph(layout, df1 %>% filter(generation == time_point))
   other_data_layout <- add_data_to_graph(layout, df2 %>% filter(generation == time_point))
-  ggraph(data_layout) + geom_edge_link(start_cap = circle(5, 'mm'), end_cap = circle(5, 'mm')) + geom_node_circle(aes(r=sqrt(pop)/100), fill="blue", color="blue", alpha=.5, stroke=0) + geom_node_circle(data= other_data_layout, aes(r=sqrt(pop)/100), fill="red", color="red",alpha=.5, stroke=0) + theme_graph(background = "white") + geom_node_text(aes(label=label))
+  ggraph(data_layout) + geom_edge_link(start_cap = circle(5, 'mm'), end_cap = circle(5, 'mm')) + geom_node_circle(aes(r=sqrt(pop)/100), fill="blue", color="blue", alpha=.5, linetype="blank") + geom_node_circle(data= other_data_layout, aes(r=sqrt(pop)/100), fill="red", color="red",alpha=.5, linetype="blank") + theme_graph(background = "white") + geom_node_text(aes(label=label))
+}
+
+draw_box_and_whiskers_animation <- function(filenameglob, lower_time_bound, upper_time_bound, frame_freq = 10) {
+  fileNames <- Sys.glob(filenameglob)
+  df <- data.frame()
+  for (file in fileNames) {
+    df <- rbind(df, read_csv(file))
+  }
+  
+  median_df <- df %>% gather(-generation, key="name", value="popsize") %>% group_by(generation, name) %>% summarise(pop=median(popsize))
+  innerring_df <- df %>% gather(-generation, key="name", value="popsize") %>% group_by(generation, name) %>% summarise(pop=quantile(popsize)[2])
+  outerring_df <- df %>% gather(-generation, key="name", value="popsize") %>% group_by(generation, name) %>% summarise(pop=quantile(popsize)[4])
+  
+  
+  median_df$name <- str_replace(median_df$name, "pop", "V")
+  median_df$name <- str_replace(median_df$name, "[:digit:]+", function(x){return(as.numeric(x)+1)})
+  
+  innerring_df$name <- str_replace(innerring_df$name, "pop", "V")
+  innerring_df$name <- str_replace(innerring_df$name, "[:digit:]+", function(x){return(as.numeric(x)+1)})
+  
+  outerring_df$name <- str_replace(outerring_df$name, "pop", "V")
+  outerring_df$name <- str_replace(outerring_df$name, "[:digit:]+", function(x){return(as.numeric(x)+1)})
+  
+  median_layout <- add_data_to_graph(layout, median_df %>% filter(generation > lower_time_bound) %>% filter(generation < upper_time_bound) %>% filter(generation  %% frame_freq == 0))
+  innerring_layout <- add_data_to_graph(layout, innerring_df%>% filter(generation > lower_time_bound) %>% filter(generation < upper_time_bound) %>% filter(generation  %% frame_freq == 0))
+  outerring_layout <- add_data_to_graph(layout, outerring_df%>% filter(generation > lower_time_bound) %>% filter(generation < upper_time_bound) %>% filter(generation  %% frame_freq == 0))
+  
+  ggraph(median_layout) + geom_edge_link(start_cap = circle(5, 'mm'), end_cap = circle(5, 'mm')) + geom_node_circle(data=outerring_layout, aes(r=log(pop+1)/10), fill="white", color="blue") + geom_node_circle(data= median_layout, aes(r=log(pop+1)/10),color="white")+ geom_node_circle(data= innerring_layout, aes(r=log(pop+1)/10), color="red") + theme_graph(background = "white") + geom_node_text(aes(label=label)) + transition_time(time)
 }
 
 # Uncomment this line to make a temporal animation
@@ -89,5 +117,7 @@ make_circle_overlay <- function(filename1, filename2, time_point) {
 
 # This line makes a snapshot
 #make_snapshot(filename1, time_point)
+
+#draw_box_and_whiskers_animation("../data/90*/pop_sizes.csv", lower_time_bound, upper_time_bound)
 
 make_circle_overlay(filename1, filename2, time_point)
